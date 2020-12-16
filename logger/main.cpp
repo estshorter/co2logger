13bd682e6b74176d75a59bc6294892361edb8d7a
estshorter <1430311+estshorter@users.noreply.github.com>
2020-12-16 18:21:39 +0900

    formatting

--- logger/main.cpp
+++ logger/main.cpp
@@ -17,7 +17,8 @@ static std::unique_ptr<Co2meter> dev = std::make_unique<Co2meter>();
 
 void signal_handler(int sig_num)
 {
-	switch (sig_num) {
+	switch (sig_num)
+	{
 	case SIGINT:
 	case SIGTERM:
 		dev->StopMonitoring();
@@ -27,34 +28,41 @@ void signal_handler(int sig_num)
 	}
 }
 
-void server(){
+void server()
+{
 	using namespace httplib;
 	using namespace std::chrono;
 	Server svr;
 
-	svr.Get("/data", [&](const Request& req, Response& res) {
+	svr.Get("/data", [&](const Request &req, Response &res) {
 		auto temp = dev->GetTemp();
 		nlohmann::json send_data;
-		if (temp) {
+		if (temp)
+		{
 			double temp_value = temp.value().value;
 			std::ostringstream oss;
 			oss << std::fixed << std::setprecision(2) << temp.value().value;
-			int64_t  temp_time = duration_cast<seconds>(temp.value().time.time_since_epoch()).count();
-			send_data["temperature"] = { {"time", temp_time}, {"value", oss.str()} };
-		}else{
-			send_data["temperature"] = { {"time", nullptr}, {"value", nullptr} };
+			int64_t temp_time = duration_cast<seconds>(temp.value().time.time_since_epoch()).count();
+			send_data["temperature"] = {{"time", temp_time}, {"value", oss.str()}};
+		}
+		else
+		{
+			send_data["temperature"] = {{"time", nullptr}, {"value", nullptr}};
 		}
 		auto co2 = dev->GetCo2();
-		if (co2) {
+		if (co2)
+		{
 			int co2_value = co2.value().value;
-			int64_t  co2_time =  duration_cast<seconds>(co2.value().time.time_since_epoch()).count();
-			send_data["co2"] = { {"time", co2_time}, {"value", co2_value} };
-		}else{
-			send_data["co2"] = { {"time", nullptr}, {"value", nullptr} };
+			int64_t co2_time = duration_cast<seconds>(co2.value().time.time_since_epoch()).count();
+			send_data["co2"] = {{"time", co2_time}, {"value", co2_value}};
+		}
+		else
+		{
+			send_data["co2"] = {{"time", nullptr}, {"value", nullptr}};
 		}
 		res.set_header("Access-Control-Allow-Origin", "*");
 		res.set_content(send_data.dump(), "application/json");
-			});
+	});
 	svr.listen("localhost", 31906);
 }
 
@@ -90,15 +98,13 @@ int main(int argc, char *argv[])
 	}
 
 	//std::cout << config.dump() << std::endl;
-	if (config["channel_id"] == nullptr || config["write_key"] == nullptr 
-		|| config["monitoring_cycle_seconds"] == nullptr 
-		|| config["reporting_cycle_seconds"] == nullptr)
+	if (config["channel_id"] == nullptr || config["write_key"] == nullptr || config["monitoring_cycle_seconds"] == nullptr || config["reporting_cycle_seconds"] == nullptr)
 	{
 		std::cerr << "contents of the json file is wrong" << std::endl;
 		return 1;
 	}
 
-	auto th1 = std::thread([]{ server(); });
+	auto th1 = std::thread([] { server(); });
 
 	int channel_id = config["channel_id"];
 	httplib::Client cli("ambidata.io", 80);
@@ -114,7 +120,7 @@ int main(int argc, char *argv[])
 
 	int err_cnt = 0;
 	constexpr int err_threshold = 10;
-	while(true)
+	while (true)
 	{
 		std::this_thread::sleep_for(std::chrono::seconds(config["reporting_cycle_seconds"]));
 		auto temp = dev->GetTemp();
@@ -138,7 +144,8 @@ int main(int argc, char *argv[])
 							  << "response code: " << res->status << std::endl;
 					err_cnt++;
 				}
-				else {
+				else
+				{
 					err_cnt = 0;
 				}
 			}
@@ -148,7 +155,8 @@ int main(int argc, char *argv[])
 				err_cnt++;
 			}
 			// network error detected
-			if (err_cnt >= err_threshold) {
+			if (err_cnt >= err_threshold)
+			{
 				dev->StopMonitoring();
 				Co2meter::Exit();
 				return 1;
